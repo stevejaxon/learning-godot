38aa5f73bbf23994b31736e3177f47f2d1b42cd1
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-12-06 18:07:57 -0800

    feat(gui) dynamic gui tutorial start

---
+++ dynamic-gui/scripts/GUI.gd
@@ -0,0 +1,8 @@
+extends MarginContainer
+
+onready var number_label = $Bars/LifeBar/Count/Background/Number
+onready var bar = $Bars/LifeBar/TextureProgress
+onready var tween = $Tween
+
+func _ready():
+	pass
