739ac4c5aa6d79ed2a84e9d9bc3da9690c1614ef
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-12-21 13:35:50 -0800

    feat(healthbar): healthbar now hidden until unit takes damage

--- tower-defense/scripts/ui/HealthDisplay.gd
+++ tower-defense/scripts/ui/HealthDisplay.gd
@@ -4,14 +4,19 @@ var bar_full_health = preload("res://assets/ui/barHorizontal_green_mid 200.png")
 var bar_taken_damage = preload("res://assets/ui/barHorizontal_yellow_mid 200.png")
 var bar_low_health = preload("res://assets/ui/barHorizontal_red_mid 200.png")
 
+func _ready():
+	$HealthBar.hide() # Start off with the healtbar hidden when units are on full health
+
 func _process(delta):
 	global_rotation = 0 # prevent the health bar rotating with any attached units
 
 func update_health_bar(value):	
 	if value < 25:
 		$HealthBar.texture_progress = bar_low_health
-	elif value < 100:
+	elif value < 60:
 		$HealthBar.texture_progress = bar_taken_damage
 	else:
-		$HealthBar.texture_progress = bar_full_health	
+		$HealthBar.texture_progress = bar_full_health
+		if value < 100:
+			$HealthBar.show()
 	$HealthBar.value = value
