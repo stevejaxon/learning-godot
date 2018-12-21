9f207f38a115dcc517b5c924622e4ef9674188d0
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-12-21 13:05:39 -0800

    feat(healthbars): adding assets for healthbars

--- tower-defense/scripts/ui/HealthDisplay.gd
+++ tower-defense/scripts/ui/HealthDisplay.gd
@@ -1,18 +1,17 @@
 extends Node2D
 
-#var bar_low_health = preload("")
-#var bar_full_health = preload("")
-#var bar_taken_damage = preload("")
+var bar_low_health = preload("res://assets/ui/barHorizontal_green_mid 200.png")
+var bar_full_health = preload("res://assets/ui/barHorizontal_red_mid 200.png")
+var bar_taken_damage = preload("res://assets/ui/barHorizontal_yellow_mid 200.png")
 
 func _process(delta):
 	global_rotation = 0 # prevent the health bar rotating with any attached units
 
-func update_health_bar(value):
-	# $HealthBar.texture_progress = bar_full_health
+func update_health_bar(value):	
 	if value < 25:
-		# TODO
-		$HealthBar.texture_progress 
+		$HealthBar.texture_progress = bar_low_health
 	elif value < 100:
-		# TODO
-		$HealthBar.texture_progress
+		$HealthBar.texture_progress = bar_taken_damage
+	else:
+		$HealthBar.texture_progress = bar_full_health	
 	$HealthBar.value = value
