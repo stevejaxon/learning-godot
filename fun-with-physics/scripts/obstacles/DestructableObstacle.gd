897782221bcb50399ff48c53a4e6cc8aeb3e5144
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2019-01-04 14:03:00 -0800

    feat(obstacles): improving the animation of destructable objects

--- fun-with-physics/scripts/obstacles/DestructableObstacle.gd
+++ fun-with-physics/scripts/obstacles/DestructableObstacle.gd
@@ -1,14 +1,13 @@
 extends StaticBody2D
 
 func _on_Area2D_body_entered(body):
-	print(body)
-	print("FOO")
+	_animate_destroyed()
+
+func _animate_destroyed():
 	$AnimationPlayer.play("destroyed")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	print("finished")
 	_clean_up()
 
 func _clean_up():
-	print("clean")
 	queue_free()
