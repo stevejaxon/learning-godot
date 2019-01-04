62ed6220954641c40c44164465d0d07c43c8fdac
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2019-01-04 13:35:37 -0800

    feat(obstacles): destructable objects are now destroyed on contract

--- fun-with-physics/scripts/obstacles/DestructableObstacle.gd
+++ fun-with-physics/scripts/obstacles/DestructableObstacle.gd
@@ -2,5 +2,13 @@ extends StaticBody2D
 
 func _on_Area2D_body_entered(body):
 	print(body)
-	print("POW")
-	pass # replace with function body
+	print("FOO")
+	$AnimationPlayer.play("destroyed")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	print("finished")
+	_clean_up()
+
+func _clean_up():
+	print("clean")
+	queue_free()
