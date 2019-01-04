13d28d61ed278a1163dc7f2536b82b0c35296bf0
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2019-01-04 13:42:34 -0800

    fix: resolving an issue with negative rotational velocity

--- fun-with-physics/scripts/projectiles/Projectile.gd
+++ fun-with-physics/scripts/projectiles/Projectile.gd
@@ -1,11 +1,12 @@
 extends RigidBody2D
 
 func _physics_process(delta):
+	print(angular_velocity)
 	if _has_shot_petered_out():
 		_trigger_disintigrate()
 	
 func _has_shot_petered_out():
-	return angular_velocity > 0 && angular_velocity < 5
+	return angular_velocity > 0 && angular_velocity < 1 or angular_velocity < 0 && angular_velocity > -1
 	
 func _trigger_disintigrate():
 	_clean_up()
