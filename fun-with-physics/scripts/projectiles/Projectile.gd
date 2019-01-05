1b8fe7d25e7875ff56cb1ce1577db349eeca8a31
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2019-01-04 17:05:16 -0800

    feat(obstacles): improving detection of the end of the shot

--- fun-with-physics/scripts/projectiles/Projectile.gd
+++ fun-with-physics/scripts/projectiles/Projectile.gd
@@ -1,16 +1,20 @@
 extends RigidBody2D
 
+const min_velocity_delta = Vector2(0.1, 2)
+
+var previous_velocity = Vector2()
+
 func _physics_process(delta):
 	if _has_shot_petered_out():
 		_trigger_disintigrate()
 	
 func _has_shot_petered_out():
-	print(linear_velocity)
-	print(angular_velocity)
-	if angular_velocity > 0:
-		return angular_velocity < 1
-	elif angular_velocity < 0: 
-		return angular_velocity > -1
+	if previous_velocity.x == 0 && previous_velocity.y == 0:
+		previous_velocity = linear_velocity.abs()
+	else:	
+		var delta = previous_velocity - linear_velocity.abs()
+		previous_velocity = linear_velocity.abs()
+		return delta.x > 0 && delta < min_velocity_delta
 	
 func _trigger_disintigrate():
 	_clean_up()
