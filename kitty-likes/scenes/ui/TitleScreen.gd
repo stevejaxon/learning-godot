638af89f01e74eb76755bbe5e68808f10f9538a3
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-02-25 22:05:43 -0800

    feat(title screen): initial prototype of the title screen

---
+++ kitty-likes/scenes/ui/TitleScreen.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _ready():
+	for button in $Menu/CenterRow/Buttons.get_children():
+		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+		
+func _on_Button_pressed(scenes):
+	get_tree().change_scene(scenes)
