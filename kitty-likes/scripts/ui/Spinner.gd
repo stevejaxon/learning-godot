cb8d74ca39ab208a0459e256595c21df187c2a3a
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2019-02-28 15:19:23 -0800

    feat(spinner): reworking the spinner animation

--- kitty-likes/scripts/ui/Spinner.gd
+++ kitty-likes/scripts/ui/Spinner.gd
@@ -1,65 +1,79 @@
-extends Node2D
+extends Control
 
-# The spinner works on a 3x3 grid of numbers from 0 - 7 around a position e.g. if the player is in the grid position X then the position is numbered as such:
+# The spinner works on a hexadecemal, isometric grid of numbers from 0 - 5 around a position e.g. if the player is in the position X then the position is numbered as such:
 #
-# 0 1 2
-# 7 X 3
-# 6 5 4
+# 4   5
+# 3 X 0
+# 2   1
 #
 # A random number in this range will decide which direction the player moves next turn.
 
-# Constants
-const FAST_ROTATION_SPEED = PI/8
-const FAST_NUM_QUADRANTS = 48 # 3 complete rotations
-const MEDIUM_ROTATION_SPEED = PI/16
-const MEDIUM_NUM_QUADRANTS = 112 # FAST (48) + MEDIUM (64)
-const SLOW_ROTATION_SPEED = PI/32
-const SLOW_NUM_QUADRANTS = 176 # FAST (48) + MEDIUM (64) + SLOW (64)
-const SLOW_TO_A_STOP_ROTATION_SPEED =PI/64
-
-var total_num_quadrants = 0
-var num_slow_quadrants;
-var num_slow_to_a_stop_quadrants;
-var random_result;
-
-func _init():
-	randomize()
-	random_result = randi() % 7 # Random number in the range 0 - 7 inclusive
-	prints("random number", random_result)
-	num_slow_quadrants = SLOW_NUM_QUADRANTS +  _calculate_required_quadrants_at_slow_speed(random_result)
-	num_slow_to_a_stop_quadrants = num_slow_quadrants + 16
-
-func _calculate_required_quadrants_at_slow_speed(result):
-	match result:
-		4: 
-			return 0
-		5: 
-			return 8
-		6: 
-			return 16
-		7:
-			return 24
-		0:
-			return 32
-		1:
-			return 40
-		2:
-			return 48
-		3:
-			return 56
-
-func _process(delta):
-	var rotate_speed;
+func _ready():
+	spin(1)
 	
-	if total_num_quadrants < FAST_NUM_QUADRANTS:
-		rotate_speed = FAST_ROTATION_SPEED
-	elif total_num_quadrants < MEDIUM_NUM_QUADRANTS:
-		rotate_speed = MEDIUM_ROTATION_SPEED
-	elif total_num_quadrants < num_slow_quadrants:
-		rotate_speed = SLOW_ROTATION_SPEED
-	elif total_num_quadrants < num_slow_to_a_stop_quadrants:
-		rotate_speed = SLOW_TO_A_STOP_ROTATION_SPEED
-	else:
-		rotate_speed = 0
-	$Sprite.rotate(rotate_speed)
-	total_num_quadrants = total_num_quadrants + 1
+func spin(result):
+	yield(_baseSpin(), "completed")
+	yield(_finalTicks(result), "completed")
+
+func _baseSpin():
+	yield(_playFastSpin(), "completed")
+	yield(_playMediumSpin(), "completed")
+	yield(_playMediumSlowSpin(), "completed")
+	
+func _finalTicks(result):
+	match result:
+		0:
+			yield(_playSlowTick(1, 5), "completed")
+			yield(_playSlowTick(2, 5), "completed")
+			yield(_playSlowTick(3, 4), "completed")
+			yield(_playSlowTick(4, 3), "completed")
+			yield(_playSlowTick(5, 2), "completed")
+			yield(_playSlowTick(0, 1), "completed")
+		1:
+			yield(_playSlowTick(1, 1), "completed")
+		2:
+			yield(_playSlowTick(1, 2), "completed")
+			yield(_playSlowTick(2, 1), "completed")
+		3:
+			yield(_playSlowTick(1, 3), "completed")
+			yield(_playSlowTick(2, 2), "completed")
+			yield(_playSlowTick(3, 1), "completed")
+		4:
+			yield(_playSlowTick(1, 4), "completed")
+			yield(_playSlowTick(2, 3), "completed")
+			yield(_playSlowTick(3, 2), "completed")
+			yield(_playSlowTick(4, 1), "completed")
+		5:
+			yield(_playSlowTick(1, 5), "completed")
+			yield(_playSlowTick(2, 4), "completed")
+			yield(_playSlowTick(3, 3), "completed")
+			yield(_playSlowTick(4, 2), "completed")
+			yield(_playSlowTick(5, 1), "completed")
+	
+func _playFastSpin():
+	yield(_playFullSpin(15), "completed")
+	
+func _playMediumSpin():
+	yield(_playFullSpin(10), "completed")
+	
+func _playMediumSlowSpin():
+	yield(_playFullSpin(5), "completed")
+	
+func _playSlowTick(number, speed):
+	$AnimationPlayer.play(String(number), -1, speed)
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	
+func _playFullSpin(speed):
+	$AnimationPlayer.play("1", -1, speed)
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	$AnimationPlayer.play("2", -1, speed)
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	$AnimationPlayer.play("3", -1, speed)
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	$AnimationPlayer.play("4", -1, speed)
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	$AnimationPlayer.play("5", -1, speed)
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	$AnimationPlayer.play("0", -1, speed)
+	yield(get_node("AnimationPlayer"), "animation_finished")
+	
