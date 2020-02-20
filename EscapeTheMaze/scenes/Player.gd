8354f540d2efda5fbbb45ef7173b75019317e4c2
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2020-02-19 20:57:37 -0800

    feat: characters

---
+++ EscapeTheMaze/scenes/Player.gd
@@ -0,0 +1,24 @@
+extends "res://scenes/Character.gd"
+
+signal moved
+signal dead
+signal grabbed_key
+signal win
+
+func _process(delta):
+	if can_move:
+		for dir in moves.keys():
+			if Input.is_action_pressed(dir):
+				if move(dir):
+					emit_signal("moved")
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("enemies"):
+		#yield($Lose, "finished")
+		emit_signal("dead")
+	if area.has_method("pickup"):
+		area.pickup()
+		if area.type == "key_red":
+			emit_signal("grabbed_key")
+		if area.type == "star":
+			emit_signal("win")
